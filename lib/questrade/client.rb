5a4b230ed1dd3a8eab1035aea82ea0e1f62af6c9
Vaibhav <ishuvaibhav@gmail.com>
2017-11-28 01:37:30 -0500

    client class template

---
+++ lib/questrade/client.rb
@@ -0,0 +1,19 @@
+module Questrade
+
+  class Client
+
+    def get(path)
+      return path
+    end
+
+    def post(path)
+      return path
+    end
+
+    def put(path)
+      return path
+    end
+
+  end
+
+end
