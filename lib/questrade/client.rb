e8247885b1f839c5c266659b890194ab447fda8f
Vaibhav <ishuvaibhav@gmail.com>
2017-11-29 03:20:36 -0500

    added connection to questrade server

--- lib/questrade/client.rb
+++ lib/questrade/client.rb
@@ -1,6 +1,47 @@
+require 'questrade/acccount'
+require 'faraday'
+
 module Questrade
 
+  def self.login(token)
+    url = "https://login.questrade.com/oauth2/"
+
+    connect = Faraday.new @endpoint do |f|
+      f.headers[:user_agent] = Questrade::USER_AGENT
+      f.headers['Content-length'] = '0'
+      f.response :json, content_type: /\bjson$/
+      f.adapter Faraday.default_adapter
+    end
+    endpoint = "#{url}token?grant_type=refresh_token&refresh_token=#{refresh_token}"
+    res = connect.send(:post, endpoint)
+
+    if r.success?
+      puts "Refresh token: #{res.body['refresh_token']}"
+      endpoint = res.body['api_server']
+      token = res.body['access_token']
+      return Client.new(endpoint: endpoint, token: token)
+    else
+      puts "Login failed! Error Message: "
+      puts r.body
+      return nil
+    end
+  end
+
   class Client
+
+    # allows token and endpoint to be read from object
+    attr_reader :endpoint, :token
+
+    def initialize(options = {})
+      @endpoint = options[:endpoint].to_s
+      @token = options[:token].to_s
+    end
+
+    include Questrade::Account
+
+    def connection
+      return nil
+    end
 
     def get(path)
       return path
@@ -12,6 +53,10 @@ module Questrade
 
     def put(path)
       return path
+    end
+
+    def connect
+      return nil
     end
 
   end
